c992c3e073887723b9363648419ebe55611dcca3
Makoto Watanabe <198112112+f-makotowatanabe@users.noreply.github.com>
2025-08-06 18:35:50 -0300

    Update script-hike-plusoft.js

--- script-hike-plusoft.js
+++ script-hike-plusoft.js
@@ -4,10 +4,11 @@
 // @version      1.0
 // @author       MakotoWatanabe
 // @include      https://plusoft-itsm.inpaas.com/forms-v2/bpmruntime.userflows.forms.bpm_workflow_*
-// @grant        none
-// @run-at       document-idle
+// @icon         https://hikeplatform.com/wp-content/themes/area-structure-1/assets/images/favicon.png
 // @downloadURL  https://raw.githubusercontent.com/f-makotowatanabe/hike-JS/refs/heads/main/script-hike-plusoft.js
 // @updateURL    https://raw.githubusercontent.com/f-makotowatanabe/hike-JS/refs/heads/main/script-hike-plusoft.js
+// @grant        none
+// @run-at       document-idle
 // ==/UserScript==
 
 
